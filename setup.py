95e7c85dc58eeda5d84c0c2a6cfcd7c666466aa5
kacper <kacper.lukawski@uj.edu.pl>
2016-02-21 17:21:34 +0100

    Changed version to 0.0.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.0.2",
+    version="0.0.3",
 
     descripton="A fork of Calibre, served as a library for ebooks handling",
     long_description=long_description,
